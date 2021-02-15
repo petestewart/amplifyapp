c3461a1d65120ae20c5c1f026f64959cf3e3727e
Pete Stewart <petestew@gmail.com>
2021-02-15 15:06:27 -0600

    reversed change to gitignore

--- src/index.js
+++ src/index.js
@@ -18,5 +18,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-// removed aws-exports.js from gitignore to see if this fixes build
